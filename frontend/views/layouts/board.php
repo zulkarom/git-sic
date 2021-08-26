de321e27e79ca0094177c5a801c9e5537aeba994
zulkarom <zulkarom@gmail.com>
2021-08-26 15:37:03 +0800

    fix padding

--- frontend/views/layouts/board.php
+++ frontend/views/layouts/board.php
@@ -8,40 +8,14 @@ $list = Timeline::find()->all();
 ?>
 
 
-<div class="wrapper row3" id="board">
-  <section class="hoc container clear"> 
+<div class="wrapper row3" id="board" >
+  <section class="hoc container clear" style="padding-bottom: 0px"> 
     <!-- ################################################################################################ -->
     <div class="sectiontitle" style="margin-bottom: 5px">
       <h3 class="heading" style="font-size: 30px;" >MAIN COMMITTEE</h3>
     </div>
     <div align="center"> <img src="<?=$dirAssests?>/images/advisory_board2.png" /> </div>
     
-    
-    
-    <div class="row">
-	<div class="col-md-2"></div>
-	<div class="col-md-8">
-	
-	
-	
-	<div class="container">
-
-
-</div>
-	
-	
-	
-	
-	
-	
-	
-	
-	</div>
-</div>
-
-    
-
-
 
 
     <!-- ################################################################################################ -->
