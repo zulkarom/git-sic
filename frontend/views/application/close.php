8e13f8aade7b8c2704ec8070b7593636aa8f9b78
zulkarom <zulkarom@gmail.com>
2021-10-30 08:41:45 +0800

    close application

---
+++ frontend/views/application/close.php
@@ -0,0 +1,14 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Application */
+
+$this->title = 'Application Form';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="application-create">
+    Kindly be informed that the new application has been closed.
+
+</div>
